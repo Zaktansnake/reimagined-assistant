bc44af85bfc18ce8abfd2918a7596ea1b83abe24
zaktansnake <zaktansnake@outlook.com>
2018-08-04 16:32:31 +0800

    00 - Initial Commit
    Blank project files or application

---
+++ app/src/main/java/com/example/david_chong/reimagined_assistant/HomeScreen.java
@@ -0,0 +1,13 @@
+package com.example.david_chong.reimagined_assistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
