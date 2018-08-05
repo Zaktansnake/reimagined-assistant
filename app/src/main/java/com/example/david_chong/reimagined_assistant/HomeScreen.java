6a49f35470060e2c8535c152608f6ecbd7256d95
zaktansnake <zaktansnake@outlook.com>
2018-08-05 23:45:52 +0800

    07 - Updated Home Screen with Simple UI
    Added Simple UI Buttons and Text to the home screen to display data from
    user's speech.

--- app/src/main/java/com/example/david_chong/reimagined_assistant/HomeScreen.java
+++ app/src/main/java/com/example/david_chong/reimagined_assistant/HomeScreen.java
@@ -3,23 +3,25 @@ package com.example.david_chong.reimagined_assistant;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.david_chong.reimagined_assistant.R;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 import com.example.david_chong.reimagined_assistant.viewmodel.ViewModel;
 
-import com.example.david_chong.reimagined_assistant.speech.SpeechDataObject;
-import com.example.david_chong.reimagined_assistant.speech.SpeechInterpretor;
-import com.example.david_chong.reimagined_assistant.speech.SpeechCallback;
-import com.example.david_chong.reimagined_assistant.speech.APIAIRecognizer;
-
-
 public class HomeScreen extends AppCompatActivity {
-
-    private boolean currentLocationWeather;
+    private static final String TAG = "HS";
 
     private ViewModel viewModel;
 
-    //TODO: Add Speech Interpretor to HomeScreen
+    private Button speechBtn;
+    private TextView speechOutput;
+    private TextView location;
+    private TextView weather;
+    private TextView temperature;
+    private TextView windSpeed;
+
+    //TODO: Add Speech Interpreter to HomeScreen
     //TODO: Add Weather Data Caller to HomeScreen
 
     @Override
@@ -27,7 +29,25 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        setupUI();
+
         this.viewModel = new ViewModel();
-        this.currentLocationWeather = false;
     }
+
+    private void setupUI() {
+        this.speechBtn = (Button) findViewById(R.id.speechBtn);
+        this.speechOutput = (TextView) findViewById(R.id.speechOutput);
+        this.location = (TextView) findViewById(R.id.location);
+        this.weather = (TextView) findViewById(R.id.weather);
+        this.temperature = (TextView) findViewById(R.id.temperature);
+        this.windSpeed = (TextView) findViewById(R.id.windSpeed);
+
+        this.speechBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Add Listener method to interpret speech
+            }
+        });
+    }
+
 }
