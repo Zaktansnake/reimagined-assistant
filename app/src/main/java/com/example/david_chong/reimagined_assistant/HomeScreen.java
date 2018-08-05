6fe03ec33599cab35b7250b478c8f91053a48e50
zaktansnake <zaktansnake@outlook.com>
2018-08-05 20:06:33 +0800

    04 - Added ViewModel to HomeScreen
    Added ViewModel class to HomeScreen

--- app/src/main/java/com/example/david_chong/reimagined_assistant/HomeScreen.java
+++ app/src/main/java/com/example/david_chong/reimagined_assistant/HomeScreen.java
@@ -3,11 +3,22 @@ package com.example.david_chong.reimagined_assistant;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.david_chong.reimagined_assistant.viewmodel.ViewModel;
+
+import java.math.BigDecimal;
+
 public class HomeScreen extends AppCompatActivity {
+
+    private boolean currentLocationWeather;
+
+    private ViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        this.viewModel = new ViewModel();
+        this.currentLocationWeather = false;
     }
 }
